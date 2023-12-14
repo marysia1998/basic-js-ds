1b0bc7c7a1b992e9bbd5e2270b703dc1fb189f96
unknown <m.n.volkova22@gmail.com>
2023-12-14 16:37:23 +0300

    feat:add functions for binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,158 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.base = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.base;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let node = new Node(data)
+    if(this.base == null) {
+        this.base = node
+    } else{
+        this.insertNode(node,this.base)
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  insertNode(newNode, node) {
+    if(node.data > newNode.data) {
+        if(node.left== null) {
+            node.left = newNode
+        } else {
+            this.insertNode(newNode, node.left)
+        }
+    } else {
+        if(node.right == null) {
+            node.right = newNode
+        } else {
+            this.insertNode(newNode, node.right)
+        }
+    }
+}
+
+  has(data) {
+    if(this.base == null) {
+      return false;
+  } else {
+     return this.hasNode(data, this.base)
+  }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  hasNode(data, node) {
+    if(node==null) {
+        return false;
+    }
+    if(node.data == data) {
+        return true;
+    }
+    if(node.data < data) {
+        return this.hasNode(data, node.right)
+    } else {
+        return this.hasNode(data, node.left)
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if(this.base == null) {
+      return null;
+  } else {
+     return this.findNode(data, this.base)
+  }
+  }
+
+  findNode(data, node) {
+    if(node==null) {
+      return null;
+  }
+  if(node.data == data) {
+      return node;
+  }
+  if(node.data < data) {
+      return this.findNode(data, node.right)
+  } else {
+      return this.findNode(data, node.left)
+  }
+  }
+
+  remove(data) {
+    this.base = this.removeNode(data, this.base);
+  }
+
+  removeNode(data, node) {
+    if(node == null) {
+      return null;
+    }
+
+    if(node.data > data) {
+        node.left = this.removeNode(data, node.left);
+        return node;
+
+    } else if(node.data < data){
+      node.right = this.removeNode(data, node.right)
+      return node;
+    } else if(node.data == data) {
+       if(node.left == null && node.right == null) {
+        node = null
+        return node;
+      }
+      else if(node.left == null) {
+        node = node.right
+        return node;
+      } else if(node.right == null) {
+        node = node.left
+        return node;
+
+      } else {
+        let newData = this.maxNode(node.left)
+      node.data = newData
+      node.left = this.removeNode( newData, node.left)
+      return node;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.base == null) {
+      return null;
+    }
+    if(this.base.left == null) {
+      return this.base.data
+    } else
+    {
+      return this.minNode(this.base.left)
+    }
+  }
+
+  minNode(node) {
+    if(node.left == null) {
+      return node.data
+    } else{
+      return this.minNode(node.left);
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.base == null) {
+      return null;
+    }
+    if(this.base.right == null) {
+      return this.base.data
+    } else
+    {
+      return this.maxNode(this.base.right)
+    }
+  }
+
+  maxNode(node) {
+    if(node.right == null) {
+      return node.data
+    } else{
+      return this.maxNode(node.right);
+    }
   }
 }
 
